0d3373f7733d98e1a8f229e484188150f54fc725
masinister <mason.dicicco@gmail.com>
2019-06-28 08:16:09 -0700

    network initialization

--- snn/network.py
+++ snn/network.py
@@ -1,4 +1,12 @@
+from connection import Connection
+from population import Population
+
 class Network:
 
-    def __init__(self):
-        pass
+    def __init__(self, pop, conn):
+        self.populations = pop
+        self.connections = conn
+
+    def run(steps):
+        for c in connections:
+            c.update()
