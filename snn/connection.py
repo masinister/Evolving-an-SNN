f5fda0aae6490cbeb7878187b0b0454a0bc59dd1
masinister <mason.dicicco@gmail.com>
2019-06-28 08:48:54 -0700

    schemes

--- snn/connection.py
+++ snn/connection.py
@@ -1,11 +1,22 @@
 from population import Population
-
+from schemes import Schemes
+'''
+Connection encapsulates the interaction
+between two populations of neurons.
+'''
 class Connection:
 
+    '''
+    pre: presynaptic population
+    post: postsynaptic population
+    scheme: connection scheme (e.g. random)
+    rule: method for updating the weight matrix
+    '''
     def __init__(self, pre, post, scheme, rule):
         self.pre = pre
         self.post = post
-        self.adj = []
+        self.scheme = Schemes.get(scheme)
+        self.adj = scheme(len(pre), len(post))
 
     def set_adj(self, w):
         self.adj = w
--- snn/schemes.py
+++ snn/schemes.py
@@ -2,8 +2,11 @@ import numpy as np
 
 class Schemes:
 
-    def get_scheme(name):
-        pass
-
     def random(m,n):
         return np.random.random_sample(m,n)
+
+    schemes = {"random": random,}
+
+    @staticmethod
+    def get(name):
+        return schemes[name]
