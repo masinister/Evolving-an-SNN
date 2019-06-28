72da17019b5635af81d97aafaca5ff907e1733ae
masinister <mason.dicicco@gmail.com>
2019-06-28 08:27:48 -0700

    schemes

--- snn/connection.py
+++ snn/connection.py
@@ -1,9 +1,17 @@
+from population import Population
+
 class Connection:
 
-    def __init__(self,pre,post,scheme, rule):
+    def __init__(self, pre, post, scheme, rule):
         self.pre = pre
         self.post = post
         self.adj = []
 
+    def set_adj(self, w):
+        self.adj = w
+
     def propogate(self):
         pass
+
+    def update(self):
+        # TODO Rule update adj
