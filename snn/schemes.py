32b85cb04acc76087f1cdbcf73e42ba92c6bcfd1
John Rowe <jrrowe@clemson.edu>
2019-07-01 11:19:30 -0700

    edited allBut1 rule

--- snn/schemes.py
+++ snn/schemes.py
@@ -6,9 +6,8 @@ def random(m,n):
 def all2all(m,n):
     return np.ones((m,n))
 
-def allBut1(m,n,i,j):
-    x = np.ones((m,n))
-    x[i,j] = 0
+def allBut1(m):
+    x = np.ones((m,m)) - np.eye(m)
     return x
 
 schemes = {"random": random, "all2all": all2all,"allBut1": allBut1,}
