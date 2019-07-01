3c0ea062566672567b07c51d41b0502dabe774bb
John Rowe <jrrowe@clemson.edu>
2019-07-01 11:39:22 -0700

    edits to STDP

--- snn/rules.py
+++ snn/rules.py
@@ -14,8 +14,8 @@ def STDP(trace, adj, post_activ, eta, mu, avg):
     '''
     unweighted = np.array(adj>0, dtype='int')
     w = 1 - adj
-    w = np.tensordot(w, unweighted, 1) # entries that were 0 (i.e. no connection) need to remain 0
-    w = np.power(w, mu)
+    w *= unweighted # entries that were 0 (i.e. no connection) need to remain 0
+    w = w**mu
     w = np.matmul(np.diag(post_activ), w) # only update if there is a postsynaptic spike
     x = trace - avg
     delta_w = eta * np.matmul(np.diag(x), w)
