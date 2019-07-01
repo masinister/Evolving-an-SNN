9fe11072bf0509bbf8c00aa2e0ad1e8214140de2
John Rowe <jrrowe@clemson.edu>
2019-07-01 10:47:04 -0700

    added all2all and allBut1 connection schemes

--- snn/schemes.py
+++ snn/schemes.py
@@ -3,7 +3,15 @@ import numpy as np
 def random(m,n):
     return np.random.random_sample((m,n))
 
-schemes = {"random": random,}
+def all2all(m,n):
+    return np.ones((m,n))
+
+def allBut1(m,n,i,j):
+    x = np.ones((m,n))
+    x[i,j] = 0
+    return x
+
+schemes = {"random": random, "all2all": all2all,"allBut1": allBut1,}
 
 def get(name):
         return schemes[name]
