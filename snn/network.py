4da3908954e62edc9c098ee80443dd5a51d8f445
John Rowe <jrrowe@clemson.edu>
2019-07-03 16:25:11 -0700

    moved firing rate stuff over to train

--- snn/network.py
+++ snn/network.py
@@ -4,31 +4,20 @@ from tqdm import tqdm
 import numpy as np
 
 class Network:
-    # Assumed that last population in pop list is the output layer
+    # Assumed that first population in pop list is the input population
+    # and the last population in pop list is the output population
     def __init__(self, pop, conn):
         # List of populations/connections
         self.populations = pop
         self.connections = conn
-        # firing_rates[i] is a list of average firing rates for each neuron
-        # in the ith population
-        self.firing_rates = [[] for _ in range(len(pop))]
-        i=0
-        for p in self.populations:
-            self.firing_rates[i] = np.zeros(p.num_neurons)
-            i+=1
 
 
     def run(self, steps):
         for s in tqdm(range(steps), ncols = 28*3):
             for c in self.connections:
                 c.update()
-            i=0
             for p in self.populations:
                 p.update()
-                self.firing_rates[i] += self.populations[i].activations
-                i+=1
-        for rate in self.firing_rates:
-            rate = rate/steps
 
     def set_params(self, params):
         for c in self.connections:
--- snn/train.py
+++ snn/train.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def train(network, train_data, learn_steps, rest_steps):
     i=0
@@ -9,5 +9,17 @@ def train(network, train_data, learn_steps, rest_steps):
         network.run(rest_steps)
         i+=1
 
-def associate_neurons(network, test_data, test_labels, steps):
-    pass
+def associate_neurons(network):
+    '''
+    firing_rates is a list of 2D arrays (of possibly varying shape) such that
+    firing_rates[i,j] is an array containing the 10 average firing rates
+    (cooresponding to each digit) for the jth neuron in the ith population
+    (excluding the input layer)
+
+    So if firing_rates[1,3,8] is high, that means the 3rd neuron in the
+    1st population tends to fire when the network is presented with an 8
+    '''
+    firing_rates = []
+    for pop in network.populations[1:]:
+        firing_rates.append( np.zeros((pop.num_neurons, 10)) )
+
