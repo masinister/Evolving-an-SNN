023919f70d79c351a11e74b1209ed70af0b8367d
masinister <mason.dicicco@gmail.com>
2019-07-03 11:14:32 -0700

    typo in connection

--- snn/connection.py
+++ snn/connection.py
@@ -19,7 +19,7 @@ class Connection:
         self.post = post
         self.adj = adj
         self.params = params
-        self.synapse = Synapse(self.params["decay"], self.pre.activations, rule)
+        self.synapse = Synapse(self.params, self.pre.activations, rule)
 
     '''
     update synapse and adjacency matrix then transmit weighted sums of spikes along the connection
