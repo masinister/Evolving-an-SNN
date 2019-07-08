a238c8534617334667dff6a3f0a0ad6fa5bf8a3e
John Rowe <jrrowe@clemson.edu>
2019-07-08 08:45:38 -0700

    added method to network to record number of activations for labeling

--- network.py
+++ network.py
@@ -19,6 +19,37 @@ class Network:
             for p in self.populations:
                 p.update()
 
+
+
+    def record(self, steps):
+        rates = []
+        for pop in self.populations[1:]:
+            rates.append( np.zeros(pop.num_neurons) )
+        # Assert STDP is turned off
+        for c in self.connections:
+            assert( c.params["Static"] )
+    
+        # present the image, and every time a neuron fires increment rates
+        for s in range(steps):
+            for c in self.connections:
+                c.update()
+            i = 0
+            for p in self.populations:
+                p.update()
+                if i > 0:
+                    rates[i-1] += p.activations
+                i += 1
+    
+        # Normalize each population's firing rate
+        for i in range(len(rates)):
+            rates[i] = rates[i]/np.max(rates[i])
+    
+        return rates
+
+
+
+
+
     def set_params(self, params):
         for c in self.connections:
             c.set_params(params)
--- train.py
+++ train.py
@@ -10,8 +10,7 @@ def train(network, train_data, learn_steps, rest_steps):
         network.run(rest_steps)
         i+=1
 
-def label_neurons(network, test_data, test_labels, num_labels, steps):
-    network.params["training"] = False
+def label_neurons(network, label_data, labels, show_steps, rest_steps):
     '''
     firing_rates is a list of 2D arrays (of possibly varying shape) such that
     firing_rates[i,j] is an array containing the 10 average firing rates
@@ -19,13 +18,42 @@ def label_neurons(network, test_data, test_labels, num_labels, steps):
     (excluding the input layer)
 
     So if firing_rates[1,3,8] is high, that means the 3rd neuron in the
-    1st population tends to fire when the network is presented with an 8
+    1st population tends to fire a lot when the network is presented with an 8
     '''
     firing_rates = []
     for pop in network.populations[1:]:
-        firing_rates.append( np.zeros((pop.num_neurons, num_labels)) )
-    #count spikes
-    network.neuron_labels = firing_rates
+        firing_rates.append( np.zeros((pop.num_neurons, 10)) )
+
+    '''
+    This turns off STDP. STDP is not turned back on here, so if you go through
+    cycles of training and labeling then "Static" needs to be set to False again
+    for the appropriate populations
+    '''
+    for con in network.connections:
+        con.params["Static"] = True
+
+    # For each image shown, record the number of times that the neurons fire
+    i = 0
+    for x in label_data:
+        network.populations[0].set_input(x)
+        rates = network.record(show_steps)
+        rates = np.reshape(rates,(np.size(rates),))
+        j=0
+        for r in firing_rates:
+            r[:,labels[i]] += rates[j]
+            j += 1
+        network.populations[0].set_blank()
+        network.run(rest_steps)
+        i += 1
+
+    '''
+    The firing_rates for each label need to be divided by the number of times
+    that type of object was shown to the network
+    '''
+    print(firing_rates[:][0:10])
+
+
+
 
 def evaluate(network, test_data, test_labels, num_labels, steps):
     network.accuracy = 0.0
