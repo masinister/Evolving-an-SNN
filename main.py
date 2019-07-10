882e4df726911389f9c47c3917892fdf107e9c29
John Rowe <jrrowe@clemson.edu>
2019-07-10 07:53:34 -0700

    Added all the schemes to main

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ def snn_test():
     rand = schemes.get("random")
     allBut1 = schemes.get("allBut1")
     grid = schemes.get("grid")
+    all2all = schemes.get("all2all")
+    one2one = schemes.get("one2one")
+    local = schemes.get("local")
 
     C1 = Connection(Input, L1, rand(784,64), "STDP", params)
     C2 = Connection(L1, L1, grid(8,8), "STDP", params)
@@ -41,13 +44,13 @@ def snn_test():
     network.set_params(params)
 
     print("Training")
-    train.train(network, x_train[0:256], 64, 64)
+    train.train(network, x_train[0:20], 64, 64)
 
     print("Labelling")
-    train.label_neurons(network, x_test[0:256], y_test[0:256], 10, 64, 64)
+    train.label_neurons(network, x_train[0:256], y_train[0:256], 10, 64, 64)
 
     print("Testing")
-    train.evaluate(network, x_test[1000:1256], y_test[1000:1256], 64, 64)
+    train.evaluate(network, x_train[1000:1256], y_train[1000:1256], 64, 64)
 
 
 if __name__ == '__main__':
