28703905424bad01fb1672b7f311ebfe1f1e3632
John Rowe <jrrowe@clemson.edu>
2019-07-11 14:53:19 -0700

    changed inputs to learning rules, now takes entire dictionary of params

--- rules.py
+++ rules.py
@@ -13,7 +13,7 @@ weight : current value of synaptic weight
 mu : parameter to scale the dependence delta_w on the current weight
 
 '''
-def STDP(trace, adj, post_activ, eta, mu, avg):
+def STDP(pre_tr, post_tr, adj, pre_activ, post_activ, params):
 
     '''
     unweighted : binary adjacency matrix to keep track of what connections exist
@@ -22,15 +22,15 @@ def STDP(trace, adj, post_activ, eta, mu, avg):
     unweighted = np.array(adj>0, dtype='int')
     w = 1 - adj     # subtraction changes 0 entries to 1 (creating new connections),
     w *= unweighted # elementwise multiplication is done with unweighted to fix this
-    w = np.power(w, mu)
+    w = np.power(w, params["mu"])
     # only update if there is a postsynaptic spike (0 out columns where there is no post-synap spike)
     w = np.matmul(w, np.diag(post_activ))
-    x = trace - avg
-    delta_w = eta * np.matmul(np.diag(x), w)
+    x = pre_tr - params["avg"]
+    delta_w = params["eta"] * np.matmul(np.diag(x), w)
     return delta_w
 
 
-def PPrule(pre_tr, post_tr, adj, pre_activ, post_activ, eta, mu):
+def PPrule(pre_tr, post_tr, adj, pre_activ, post_activ, params):
     '''
     dw = -eta * post_tr * w * (1 - w)    if pre or (pre and post) spike
     dw = mu * pre_tr * w * (1 - w)       if post and (not pre) spike
@@ -51,16 +51,16 @@ def PPrule(pre_tr, post_tr, adj, pre_activ, post_activ, eta, mu):
     # this affects weights along the row of the adj matrix so we multiply on the right
     # by pre_activ
     dw1 = np.dot( np.diag(pre_activ), dw )
-    dw1 = -eta*np.dot( np.diag(pre_tr), dw )
+    dw1 = -params["eta"]*np.dot( np.diag(pre_tr), dw )
     # Entries are nonzero iff post and (not pre) is True
     # diag( notPre ) * dw * diag( post_activ )
     dw2 = np.dot( notPre, np.dot(dw, np.diag(post_activ)) )
-    dw2 = mu*np.dot( dw, np.diag(post_tr) )
+    dw2 = params["mu"]*np.dot( dw, np.diag(post_tr) )
 
     return dw1 + dw2
 
 
-def static(pre_tr, post_tr, adj, pre_activ, post_activ, eta, mu):
+def static(pre_tr, post_tr, adj, pre_activ, post_activ, params):
     return 0
 
 
