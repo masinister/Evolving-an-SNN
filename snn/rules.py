e1c2eb28b8504b0c8da914dbaa32e38c79b31f84
masinister <mason.dicicco@gmail.com>
2019-08-08 14:57:13 -0700

    removed old STDP rule

--- snn/rules.py
+++ snn/rules.py
@@ -1,32 +1,5 @@
 import numpy as np
 
-'''
-STDP: Time and Weight Dependent Rule
-delta_w = eta*(pre_trace - avg)(1-weight)**mu
-
-delta_w : change in synaptic weight
-eta : parameter to scale magnitude of delta_w (aka learning rate)
-pre_trace : record of presynaptic spikes
-avg : target average presynaptic trace
-weight : current value of synaptic weight
-mu : parameter to scale the dependence delta_w on the current weight
-
-'''
-def STDP(pre_tr, post_tr, adj, pre_activ, post_activ, params):
-
-    '''
-    unweighted : binary adjacency matrix to keep track of what connections exist
-    w : matrix representing the weight dependent part of delta_w
-    '''
-    w = adj*(1 - adj)
-    w = np.power(w, params["mu"])
-    # only update if there is a postsynaptic spike (0 out columns where there is no post-synap spike)
-    w = np.dot( w, np.diag(post_activ))
-    x = pre_tr - params["avg"]
-    delta_w = params["eta"] * np.dot( np.diag(x), w)
-    return delta_w
-
-
 def PreAndPost(pre_tr, post_tr, adj, pre_activ, post_activ, params):
     '''
     dw = -eta * post_tr * w * (1 - w)    if pre or (pre and post) spike
@@ -49,7 +22,7 @@ def static(pre_tr, post_tr, adj, pre_activ, post_activ, params):
     return 0
 
 
-rules = {"STDP": STDP,"static": static, "PreAndPost": PreAndPost}
+rules = {"static": static, "PreAndPost": PreAndPost}
 
 def get(name):
     return rules[name]
