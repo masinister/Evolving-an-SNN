414ec42388a0e07e8a05ec9cec940083f6c5686f
masinister <mason.dicicco@gmail.com>
2019-08-09 07:54:10 -0700

    removed unnecessary input scaling

--- tests/diehl-and-cook.py
+++ tests/diehl-and-cook.py
@@ -53,8 +53,6 @@ L1 = population.Population(
 '''
 Initialize connections
 '''
-for x in x_train:
-    x = x.astype(float) * 2
 
 inh = -120
 C1 = Connection(Input,
@@ -76,9 +74,9 @@ C2 = Connection(L1,
 
 network = Network([Input, L1,], [C1, C2,])
 
-# plotter = Plotter(["trace"])
+# plotter = Plotter(["voltage"])
 
 outer = tqdm(total = 100, desc = 'Epochs', position = 0)
 for i in range(100):
-    train.all_at_once(network, x_train[1000 * i: 1000 * (i+1)], y_train[1000 * i: 1000 * (i+1)], 10, 100, draw_weights = False, plot = False)
+    train.all_at_once(network, x_train[1000 * i: 1000 * (i+1)], y_train[1000 * i: 1000 * (i+1)], 10, 300, draw_weights = True, plot = False)
     outer.update(1)
