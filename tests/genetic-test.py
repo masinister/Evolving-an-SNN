7ae4e09c47a20b9d41b6005b86b19b19f0111ebb
masinister <mason.dicicco@gmail.com>
2019-08-09 07:56:24 -0700

    .gitignore bug

--- tests/genetic-test.py
+++
@@ -1,7 +0,0 @@
-from genetic.genetic import Optimizer
-
-if __name__ == '__main__':
-    generations = 1000
-    population = 8
-    optimizer = Optimizer(2)
-    optimizer.run(generations, population)
